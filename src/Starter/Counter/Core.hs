32c1a5f7501d825ac1120d40bab2629a61e4334d
NeoCLI <neo@example.com>
2026-06-11 16:12:29 +0100

    bump

--- src/Starter/Counter/Core.hs
+++
@@ -1,9 +0,0 @@
--- | Convenience re-export so callers can write `import Starter.Counter.Core`
--- instead of importing Entity and Event separately.
-module Starter.Counter.Core (
-  module Starter.Counter.Entity,
-  module Starter.Counter.Event,
-) where
-
-import Starter.Counter.Entity
-import Starter.Counter.Event
